5fe22bde85021d6c8ec38771ae01af0f7862f16f
roni4444 <debapriyopaul.dp@gmail.com>
2021-07-24 22:28:55 +0530

    Add files via upload

---
+++ src/main/java/edu/tint/project/PortfolioWebsite/HelloApplication.java
@@ -0,0 +1,9 @@
+package edu.tint.project.PortfolioWebsite;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
